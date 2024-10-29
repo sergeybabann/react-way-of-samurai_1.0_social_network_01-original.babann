5f7b1a9bc318a31419beb40c757afa49562a969b
sergeybabann <sergeybabann@yandex.ru>
2024-10-29 21:14:41 +0300

    first push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return <nav className='nav'>
+        <div className="item">
+            <a>Profile</a>
+        </div>
+        <div>
+            <a>Messages</a>
+        </div><div>
+            <a>News</a>
+        </div>
+        <div>
+            <a>Music</a>
+        </div>
+        <div>
+            <a>Settings</a>
+        </div>
+    </nav>
+}
+
+export default Navbar;
