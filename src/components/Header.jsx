ad5fac5452eea172ac36abe68d948b9fac7c64d8
sergeybabann <sergeybabann@yandex.ru>
2024-10-30 01:01:21 +0300

    14 lessons complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    return <header className={s.header}>
+        <img src="../icons/logo.png" width="50px" height="50px" alt=""/>
     </header>
 }
 
 export default Header;
+
+
+
