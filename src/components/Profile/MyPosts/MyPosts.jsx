778a2b10073634c5a4bc966c4be326eecd718dcd
sergeybabann <sergeybabann@yandex.ru>
2024-10-31 12:53:03 +0300

    16 lesson complete

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
   return (
     <div>
       <textarea></textarea>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,10 @@ const Profile = () => {
   return (
     <div className={s.content}>
       <div>
-        <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+        <img
+          src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'
+          alt=''
+        />
       </div>
       <div>ava + description</div>
 
